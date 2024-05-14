c2ff2f5f736759f02f9c3391b9e4bcaf6bf0375d
XXXPOOJA <rr5440547@gmail.com>
2024-05-14 19:13:15 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI.js
@@ -0,0 +1,29 @@
+ const fs = require("fs");
+module.exports.config = {
+	name: "aadi",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Arun", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("owner") ||
+     react.includes("aadi") || 
+react.includes("𝗔𝗔𝗗𝗜 𝗕𝗥𝗢𝗞𝗘𝗡")) {
+		var msg = {
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦🌸===『*★🌸===आदि बाबू✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 :\n\n✦ https://www.instagram.com/aadi_singh__143?igsh=MXVneXZ6bzR4bHhvcg==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 😁\n 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 𝐋𝐔𝐊 😁😋 https://www.facebook.com/profile.php?id=100083551732772&mibextid=ZbWKwL`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
