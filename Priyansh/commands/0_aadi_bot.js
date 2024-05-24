8a5be955ec93b254eaf933e8c727b01128584aa2
aadibot121314 <aadibot121314@gmail.com>
2024-05-24 06:53:51 +0530

    Update 0_aadi_bot.js

--- Priyansh/commands/0_aadi_bot.js
+++ Priyansh/commands/0_aadi_bot.js
@@ -48,7 +48,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("️️️𝐓𝐢𝐫𝐜𝐡𝐢 𝐧𝐚𝐳𝐚𝐫𝐢𝐲𝐚 𝐦𝐨𝐫𝐢 𝐡𝐚𝐚𝐲𝐞 𝐡𝐚𝐚𝐲𝐞 𝐡𝐚𝐚𝐲𝐞 🙈", threadID);
    };
   
-  if ((event.body.toLowerCase() == "BAbu") || (event.body.toLowerCase() == "BAby")) {
+  if ((event.body.toLowerCase() == "KavYa") || (event.body.toLowerCase() == "Ji")) {
      return api.sendMessag("𝐇𝐌𝐌 𝐁𝐀𝐁𝐘 😘😗", threadID);
    };
 
