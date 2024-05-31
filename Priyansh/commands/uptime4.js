12ef4302d746c8230693e766ead25e4c1d2556de
aadibot121314 <aadibot121314@gmail.com>
2024-05-31 11:06:50 +0530

    Update commands

---
+++ Priyansh/commands/uptime4.js
@@ -0,0 +1,54 @@
+module.exports.config = {
+	name: "uptime4",
+	version: "1.0.1", 
+	hasPermssion: 0,
+	credits: "Joshua Sy", //don't change the credits please
+	description: "uptime",
+	commandCategory: "uptime",
+	cooldowns: 2,
+	dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+  const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+	const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("➤D/MM/YYYY HH:mm:ss");
+	const timeStart = Date.now();
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+var link = ["https://i.imgur.com/D2UV0Dy.jpeg"];
+var callback = () => api.sendMessage({body:`➳Bot and Admin Information
+
+➜༻𝐎𝐖𝐍𝐄𝐑:- ☞𝗕𝗥𝗢𝗞𝗘𝗡 - 𝗔𝗔𝗗𝗜☜ ༺
+༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠𝗕𝗥𝗢𝗞𝗘𝗡 - 𝗔𝗔𝗗𝗜〠.༒
+
+༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/bijayani.sahoo.98?mibextid=ZbWKwL ☜ 
+
+༻𝘠𝘰𝘶𝘵𝘶𝘣𝘦 𝘭𝘪𝘯𝘬༺:- 
+☞ https://www.youtube.com/@Aadibrand-hh6gc ☜
+
+֎𝘍𝘰𝘳 𝘈𝘯𝘺 𝘒𝘪𝘯𝘥 𝘖𝘧 𝘏𝘦𝘭𝘱 𝘫𝘰𝘪𝘯 𝘞𝘱 𝘎𝘳𝘰𝘶𝘱֍:-
+
+☞ NO TQ ☜
+
+ ֎𝘊𝘰𝘯𝘵𝘢𝘤𝘵 𝘔𝘦 𝘖𝘯 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱֍ :-  ☞ 9548950035 ☜
+
+»Bot Name : 【${global.config.BOTNAME}】
+
+⫸Bot Prefix : 》${global.config.PREFIX}《
+
+➼Bot work : ➺${hours} : ${minutes} : ${seconds}
+
+➺Today Is : ${juswa}
+
+►Thanks for using ${global.config.BOTNAME}`,attachment: fs.createReadStream(__dirname + "/cache/juswa.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.gif")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.gif")).on("close",() => callback());
+   };
