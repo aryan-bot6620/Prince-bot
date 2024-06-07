2d6b5e2bd257cf273896f9f6d9ed23272b129023
aadibot121314 <aadibot121314@gmail.com>
2024-06-08 04:24:44 +0530

    Update 0_AADI.js

--- Priyansh/commands/0_AADI.js
+++ Priyansh/commands/0_AADI.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	let react = event.body.toLowerCase();
 	if(react.includes("owner ") ||
      react.includes("aadi") || 
-react.includes("BRoKen")) {
+react.includes("@Tabahi Sherni")) {
 		var msg = {
 				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦🌸===『*★🌸===आदि बाबू✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 :\n\n✦ https://www.instagram.com/aadi_singh__143?igsh=MXVneXZ6bzR4bHhvcg==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 😁\n 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 𝐋𝐔𝐊 😁😋 https://www.facebook.com/profile.php?id=100083551732772&mibextid=ZbWKwL`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
