6b6a4c0b0a5e27ea4f368759afcd5760ff1bccb7
aadibot121314 <aadibot121314@gmail.com>
2024-06-19 22:09:03 +0530

    Update fbautodownload.js

--- Priyansh/commands/fbautodownload.js
+++ Priyansh/commands/fbautodownload.js
@@ -36,7 +36,7 @@ module.exports.handleEvent = async function({ api, event }) {
                 const attachment = fs.createReadStream(tempFilePath);
                 await api.sendMessage({
                     attachment,
-                    body: "Here's the video you requested:"
+                    body: "--『 ⟬ 𝗕𝗥𝗢𝗞𝗘𝗡 - 𝗔𝗔𝗗𝗜 ꪹ 🄱🄾🅃 』--  Here's the video you requested:"
                 }, event.threadID, (err) => {
                     if (err) console.error("Error sending message:", err);
                 });
