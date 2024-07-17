81a992e2dff1f44b4a1b2446eb829f108c64b216
aryan-bot6620 <patelazaan17@gmail.com>
2024-07-17 11:21:03 +0530

    Update 0_girl_videos.js

--- Priyansh/commands/0_girl_videos.js
+++ Priyansh/commands/0_girl_videos.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["-🌸===『*★𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 𒁍आदि बाबू"];
+   var hi = ["-🌸===『*★𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 𒁍✬✿╭┳✪✪╤───────────➛➣ ★�🆁🅸🅽🅲🅴★🆂🅸🅻🅴🅽🆃)✪��🆈★  ✬✿╭┳✪✪╤───────────➛➣"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
