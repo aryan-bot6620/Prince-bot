c147f1556ccd8286354e46d6f79813f838d7cc7c
aryan-bot6620 <patelazaan17@gmail.com>
2024-07-17 10:57:28 +0530

    Update 0_AADI.js

--- Priyansh/commands/0_AADI.js
+++ Priyansh/commands/0_AADI.js
@@ -17,13 +17,12 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("aadi") || 
 react.includes("sir")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦🌸===『*★🌸===✦\n\n★★᭄==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 😁\n 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 𝐋𝐈𝐍𝐊😁😋 https://www.facebook.com/profile.php?id=61551474205816`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★✬✿╭┳✪✪╤───────────➛➣ ★�🆁🅸🅽🅲🅴★🆂🅸🅻🅴🅽🆃)✪��🆈★  ✬✿╭┳✪✪╤───────────➛➣\n\n✦🌸===『*★🌸===✦\n\n★★᭄==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 😁\n 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 𝐋𝐈𝐍𝐊😁😋 https://www.facebook.com/profile.php?id=61551474205816`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
-		}✬✿╭┳✪✪╤───────────➛➣ ★�🆁🅸🅽🅲🅴★🆂🅸🅻🅴🅽🆃)✪��🆈★  ✬✿╭┳✪✪╤───────────➛➣
-	}
+		}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
