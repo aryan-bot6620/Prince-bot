56f532b8e0c6ed79ae88b9b2ac5c6cbefeef16ba
aryan-bot6620 <patelazaan17@gmail.com>
2024-07-18 06:53:40 +0530

    Update commands

---
+++ Priyansh/commands/#hrt.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "hrt",
+    version: "1.1.1",
+  hasPermssion: 0,
+  credits: "𝐏𝐑𝐈𝐘𝐀𝐍𝐒𝐇𝐈 𝐊𝐀𝐔𝐑", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("❤️") ||
+     react.includes("🥰") || react.includes("😍") || react.includes("😻") ||
+react.includes("♥️") ||
+react.includes("💝")) {
+    var msg = {
+        body: `❤️❤️❤️❤️`,
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("❤️ ", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
